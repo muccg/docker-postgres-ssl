a0758b1bb98970efb5193062f872c43031fb91be
aahunter <ahunter@ccg.murdoch.edu.au>
2016-09-26 16:16:56 +0800

    use ssl-cert package #1

--- build.sh
+++ build.sh
@@ -5,24 +5,35 @@
 
 # break on error
 set -e
+set -x
+set -a
+: ${DOCKER_USE_HUB:="0"}
 
-REPO="muccg"
 DATE=`date +%Y.%m.%d`
 
-image="${REPO}/postgres-ssl"
+
+ci_docker_login() {
+    docker login -u ${DOCKER_USERNAME} --password="${DOCKER_PASSWORD}"
+}
+
+
 version="9.4"
+image="muccg/postgres-ssl:${version}"
 echo "################################################################### ${image}"
-        
+
 ## warm up cache for CI
 docker pull ${image} || true
 
 ## build
-docker build --pull=true -t ${image}:${DATE} .
-docker build -t ${image}:${version} .
+docker build --pull=true -t ${image}-${DATE} .
+docker build -t ${image} .
 
 ## for logging in CI
-docker inspect ${image}:${DATE}
+docker inspect ${image}-${DATE}
 
 # push
-docker push ${image}:${DATE}
-docker push ${image}:${version}
+if [ ${DOCKER_USE_HUB} = "1" ]; then
+    ci_docker_login
+    docker push ${image}-${DATE}
+    docker push ${image}
+fi
